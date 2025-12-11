2648e8708b910d025e676e4671e3008e5bae6e2c
Mathys88 <mathysviricel@gmail.com>
2025-12-11 09:24:10 +0100

    actu

--- public/styleClient.js
+++ public/styleClient.js
@@ -2,5 +2,10 @@
 // Ici tu fais ton javascript pour contrôler le changement de skin, thème etc...
 
 
+var block = document.getElementById("blockTheme")
 
 
+function chTheme(){
+    if (block.hidden === true) block.hidden = false;
+    else if (block.hidden === false) block.hidden = true;
+}
