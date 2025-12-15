9e395de185d5839a0f359a4a7246f3e25ceb4ea1
Mathys88 <mathysviricel@gmail.com>
2025-12-15 08:33:41 +0100

    .

--- public/index.js
+++ public/index.js
@@ -1,13 +1,14 @@
+
 //BACK END : Antoine
 //Je vais mettre ici toutes les fonctions (pour les boutons) que tu lieras au html, on associera le styleClient.js à ce fichier*
 
-let roomID;
+let roomID,turn;
 let pseudo,mdp;
 
 const joueur = {
     localPlayerID: "",
     pseudo: "",
-    elo: 500
+    elo: 0
 };
 
 
@@ -20,19 +21,21 @@ socket.on('connect', (data) => {
 socket.on("setLocalPlayerID",(data)=>{
     joueur.localPlayerID=data;
     console.log("localPlayerID : " + joueur.localPlayerID);
-    joueur.pseudo="Guest("+joueur.localPlayerID.substring(15)+")";
+    joueur.pseudo="Invité("+joueur.localPlayerID.substring(15)+")";
     console.log(joueur.pseudo);
 })
 
 
 
 //BDD :
+
 function deconnecter(){
     joueur.pseudo="Guest("+joueur.localPlayerID.substring(15)+")";
     joueur.elo = 0;
     document.getElementById("blockDeconnect").hidden = true
     document.getElementById("blockConnect").hidden = false;
 }
+
 
 function connexionCompte(){
     // console.log("connexionCompte");
@@ -44,13 +47,12 @@ function connexionCompte(){
 }
 
 socket.on("login_ok", (data)=>{
-
     joueur.pseudo=data.pseudo;
     joueur.elo=data.elo;
     console.log("login ok : "+data.pseudo+ " " + data.elo);
     document.getElementById("pseudo").textContent=joueur.pseudo;
-    document.getElementById("blockConnect").hidden = true;
     document.getElementById("blockDeconnect").hidden = false
+    document.getElementById("blockConnect").hidden = true;
 
 });
 
@@ -65,7 +67,7 @@ function creerCompte(){
 
 socket.on("register_ok", (data)=>{
     console.log("création de compte OK ! : " +data.pseudo + " " + data.mdp);
-    connexionCompte(data.pseudo,data.mdp);
+    connexionCompte();
 });
 
 socket.on("erreurBDD",(msg)=>{
@@ -87,10 +89,18 @@ socket.on("sendRoom", (data) => {
         pseudo : joueur.pseudo,
         localPlayerID : joueur.localPlayerID
     });
+    document.getElementById("partieG").hidden = true;
+    document.getElementById("dansPartie").hidden = false;
+    document.getElementById("dansPartieD").hidden = false;
+    document.getElementById("code").textContent = roomID.substring(4)
+    document.getElementById("InfoJ1").textContent = joueur.pseudo
 });
 
 function qClasse(){
-    socket.emit("rankedQueue",{localPlayerID: joueur.localPlayerID, elo: joueur.elo});
+    if(joueur.elo>0) socket.emit("rankedQueue",{localPlayerID: joueur.localPlayerID, elo: joueur.elo});
+    else {
+        document.getElementById("btnRanked").textContent="Se connecter pour jouer en ranked";
+    }
 }
 
 socket.on("sendRoomRanked", (data) => {
@@ -112,13 +122,46 @@ function reset(){
 
 }
 
-let code=document.getElementById("txtCode"); //ajt .value qd créé
-function joinRoom(code){
-
+ //ajt .value qd créé
+function joinRoom(){
+    let code=document.getElementById("txtCode").value;
 }
 
 
-
-function colChoix(col,pos){
-    socket.emit("choix", tabColonnes[col]);
+//fonctions du jeu en soi
+function colChoix(col){
+    socket.emit("choix", {
+        col,
+        roomID,
+        localPlayerID: joueur.localPlayerID
+    });
 }
+
+socket.on("placement",(data)=>{
+    let idPos="";
+    idPos=data.col+data.ligne;
+    let div=document.getElementById(idPos);
+    if(data.player===1) div.style.backgroundColor="red";
+    else div.style.backgroundColor="yellow";
+});
+
+socket.on("colPleine",(colonnePleine)=>{
+    console.log("colonne pleine : " + colonnePleine);
+    //afficher l'info
+});
+
+socket.on("victoire",(data)=>{
+    console.log("gagnant : " + data.pseudo + " numJoueur :" + data.gagnant);
+    //clear le client ? ou proposer un rematch ? 
+
+});
+
+socket.on("nul",()=>{
+    console.log("Match nul");
+    //clear le client
+});
+
+socket.on("tourSuivant",(tour)=>{
+    turn=tour;
+    console.log("tour du joueur : " + turn);
+});
